8f469a5a9696a194b312d3a47588e1026510fc2b
Robert Willard <robertgwillard@gmail.com>
2015-07-14 17:22:29 -0700

    naming changes

--- mock.rb
+++ mock.rb
@@ -2,24 +2,8 @@ require 'preact'
 require 'demode'
 
 class Mock
-  module Preact 
-    class << self
-      protected
-      def send_log(person, event=nil)
-        psn = person.nil? ? nil : person.to_hash
-        evt = event.nil? ? nil : event.to_hash
-
-        if defined?(Preact::Sidekiq) && (configuration.logging_mode.nil? || configuration.logging_mode == :sidekiq)
-          Preact::Sidekiq::PreactLoggingWorker.perform_async(psn, evt)
-        else
-          # use the background thread logger
-          Preact::BackgroundLogger.new.perform(psn, evt)
-        end
-      end 
-    end
-  end
   
-  def initialize(code, secret, configuration={}, defaults={})
+  def initialize(code, secret, configuration = {}, defaults= {})
     Preact.configure do |config|
       config.code = code
       config.secret = secret
@@ -51,7 +35,7 @@ class Mock
     @event_multiplier = 2
 
     if defaults && defaults.is_a?(Hash)
-      hash.each do |k,v|
+      defaults.each do |k,v|
         instance_variable_set("@#{k}", v) unless v.nil?
       end
     end
@@ -80,29 +64,6 @@ class Mock
     end
   end
 
-  def generate_random_account_events(account, event_names_count)
-    time_offset = 0
-    logs = []
-    k = rand((10 * @event_multiplier)..(100 * @event_multiplier))
-    k_init_half = k/2 + 1
-    while k > 1 do
-      account[:people].each do |person|
-        # no person should have more than half of the potential events for an account
-        j = rand([k,k_init_half].min)
-        j.times do
-          event = {
-            name: @event_names[rand(event_names_count)],
-            timestamp: Time.now.to_i - ((rand(@event_multiplier * 2)) * 24 * 3600) + time_offset
-          }
-          logs << { person: person, event: event, account: account }
-          time_offset += 1
-        end
-        k = k - j 
-      end
-    end
-    logs  
-  end
-
   def self.log_events(logs)
     logs.sort! {|x,y| x[:event][:timestamp]<=>y[:event][:timestamp]}
     logs.each do |log|
@@ -111,16 +72,60 @@ class Mock
     puts "FINISHED, IGNORE ALL ERRORS AFTER THIS"
   end
 
+  def generate_random_account_events(account, event_names_count)
+    # To not have multiple events at the exact same time
+    time_offset = 0
+    logs = []
+    #Will set event_max to at least 10*multiplier
+    event_max = rand((10 * @event_multiplier)..(100 * @event_multiplier))
+    event_max_init_half = event_max/2 + 1
+    while event_max > 1 do
+      account[:people].each do |person|
+        # no person should have more than half of the potential events for an account
+        person_events = rand([event_max, event_max_init_half].min)
+        person_events.times do
+          event = {
+            name: @event_names[rand(event_names_count)],
+            # Sets timestamp to a random subset of days, offset by time_offset
+            timestamp: Time.now.to_i - ((rand(@event_multiplier * 2)) * 24 * 3600) + time_offset
+          }
+          logs << { person: person, event: event, account: account }
+          time_offset += 1
+        end
+        event_max = event_max - person_events 
+      end
+    end
+    logs  
+  end
+
   def generate_batch
     accounts = []
     logs = []
     accounts = Mock.generate_accounts(@account_num)
     event_names_count = @event_names.count
     accounts.each do |account|
+      #Will vary the person number per account the same every execution
       account[:people] = Mock.generate_people(((account[:id] % @person_num) + 1), account[:id])
       logs << generate_random_account_events(account, event_names_count)
     end
     Mock.log_events(logs)
+  end
+
+  module Preact 
+    class << self
+      protected
+      def send_log(person, event=nil)
+        psn = person.nil? ? nil : person.to_hash
+        evt = event.nil? ? nil : event.to_hash
+
+        if defined?(Preact::Sidekiq) && (configuration.logging_mode.nil? || configuration.logging_mode == :sidekiq)
+          Preact::Sidekiq::PreactLoggingWorker.perform_async(psn, evt)
+        else
+          # use the background thread logger
+          Preact::BackgroundLogger.new.perform(psn, evt)
+        end
+      end 
+    end
   end
   # Preact.log_event(
   #   { :email       => "gooley@preact.com",
